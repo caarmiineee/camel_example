a29a7b34aeb8f9fc15e4e7370825c8312d5f7eb3
caarmiineee <carmineolivetti@gmail.com>
2024-03-11 18:56:10 +0100

    first commit

---
+++ src/main/java/com/camel_example/processor/FIrstProcessor.java
@@ -0,0 +1,14 @@
+package com.camel_example.processor;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class FIrstProcessor implements Processor {
+
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/camel_example/routes/FirstProgramRoutes.java
@@ -0,0 +1,17 @@
+package com.camel_example.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirstProgramRoutes extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		// TODO Auto-generated method stub
+		from("timer://test?period=5000")
+		.setBody(simple("Hello World!!"))
+		.to("log:test");
+	}
+
+}
